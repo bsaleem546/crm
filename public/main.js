7ba87c022901aeb9039ef9a0b9f239c826f5847e
unknown <m.bilal2145@gmail.com>
2022-08-17 18:39:58 +0500

    nav set

--- public/main.js
+++ public/main.js
@@ -2,11 +2,11 @@
 // for localhost
 // const MAINURLFORADMIN = 'http://localhost/crm-leads/admin/'
 // const MAINURLFORUSER = 'http://localhost/crm-leads/user/'
-// const MAINURL='http://localhost/crm/';
+const MAINURL='http://localhost/crm/';
 
 // for live
 // const MAINURLFORADMIN = 'https://evertsolution.com/crm/admin/'
 // const MAINURLFORUSER = 'https://evertsolution.com/crm/user/'
-const MAINURL='http://crm.evertsolution.com/';
+// const MAINURL='http://crm.evertsolution.com/';
 
 
--- resources/views/layouts/userDashboard.blade.php
+++ resources/views/layouts/userDashboard.blade.php
@@ -37,34 +37,28 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="height:100px">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    CRM
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+        <nav class="navbar navbar-expand-lg navbar-light" style="height: 100px">
+            <a class="navbar-brand" href="#">CRM</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                </ul>
+                <div class="form-inline my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Notifications
                             </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">
-                                    Notifications 1
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    Notifications 2
-                                </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
                             </div>
                         </li>
                     </ul>
@@ -72,32 +66,31 @@
             </div>
         </nav>
 
-        <nav class="navbar navbar-expand-md shadow-lg" style="background-color: #0A4D79">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav class="navbar navbar-expand-lg navbar-light shadow-lg" style="background-color: #0A4D79">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ url('/') }}">{{ __('Home') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ route('tickets.index') }}">{{ __('Support Ticket') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ url('/') }}">{{ __('Open Ticket') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ url('/') }}">{{ __('Leads') }}</a>
-                        </li>
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ url('/') }}">{{ __('Home') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ route('tickets.index') }}">{{ __('Support Ticket') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ url('/') }}">{{ __('Open Ticket') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: #FFF!important; font-size: 18px; padding: 15px" href="{{ url('/') }}">{{ __('Leads') }}</a>
+                    </li>
+                </ul>
+
+                <div class="form-inline my-2 my-lg-0">
+
+                    <ul class="navbar-nav mr-auto">
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
