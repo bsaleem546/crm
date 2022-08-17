ca172906b2a0fa8031af4056f70c78648ec4acb2
unknown <m.bilal2145@gmail.com>
2022-08-17 17:50:06 +0500

    add cros

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,27 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
+//    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+//
+//    'allowed_methods' => ['*'],
+//
+//    'allowed_origins' => ['*'],
+//
+//    'allowed_origins_patterns' => [],
+//
+//    'allowed_headers' => ['*'],
+//
+//    'exposed_headers' => [],
+//
+//    'max_age' => 0,
+//
+//    'supports_credentials' => false,
+    'paths' => ['api/*', '*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
+    'allowed_origins' => ['http://localhost', 'https://www.evertsolution.com'],
+    'allowed_origins_patterns' => ['*'],
+    'allowed_headers' => ['X-Custom-Header', 'Upgrade-Insecure-Requests', '*'],
+    'exposed_headers' => false,
+    'max_age' => false,
     'supports_credentials' => false,
-
 ];
